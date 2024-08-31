32d9c4e34d29483395204d63585e8d0d801e5b07
CHIGODI <chiegody254@gmail.com>
2024-08-31 15:02:02 +0300

    Add userscontroller logic, handling post request to add users to mongoDB

---
+++ controllers/UsersController.js
@@ -0,0 +1,37 @@
+const crypto = require('crypto');
+const dbClient = require('../utils/db');
+
+const generateSHA1Hash = (input) => {
+  const hash = crypto.createHash('sha1');
+  hash.update(input);
+  return hash.digest('hex');
+};
+
+exports.postNew = async (req, res) => {
+  const reqData = req.body;
+  const usersCollection = dbClient.client.db.collection('users');
+
+  if (!reqData.email) {
+    return res.status(400).json({ error: 'Missing email' });
+  }
+
+  if (!reqData.password) {
+    return res.status(400).json({ error: 'Missing password' });
+  }
+
+  const emailExists = await usersCollection.findOne({ email: reqData.email });
+  if (emailExists) {
+    return res.status(400).json({ error: 'Already exist' });
+  }
+
+  const hashedPwd = generateSHA1Hash(reqData.password);
+  const { insertedId } = await usersCollection.insertOne({
+    email: reqData.email,
+    password: hashedPwd,
+  });
+
+  return res.status(201).json({
+    id: insertedId,
+    email: reqData.email,
+  });
+};
