57f6d13c4e64bfc9b0545a455640bbf5a7279dfc
CHIGODI <chiegody254@gmail.com>
2024-08-31 11:45:03 +0300

    Add DBClient class, connecting to MongoDB

---
+++ utils/db.js
@@ -0,0 +1,38 @@
+#!/usr/bin/node
+/**
+ * DBClient class - connecting to MongoDB
+ */
+
+const mongoose = require('mongoose');
+
+// load environment variables
+require('dotenv').config();
+
+class DBClient {
+  constructor() {
+    const host = process.env.DB_HOST || 'localhost';
+    const port = process.env.DB_PORT || 27017;
+    const database = process.env.DB_DATABASE || 'files_manager';
+    const uri = `mongodb://${host}:${port}/${database}`;
+
+    mongoose.connect(uri);
+    this.client = mongoose.connection;
+  }
+
+  isAlive() {
+    return this.client.readyState === 1;
+  }
+
+  async nbUsers() {
+    if (!this.isAlive()) return 0;
+    return this.client.db.collection('users').countDocuments();
+  }
+
+  async nbFiles() {
+    if (!this.isAlive()) return 0;
+    return this.client.db.collection('files').countDocuments();
+  }
+}
+
+const dbClient = new DBClient();
+module.exports = dbClient;
