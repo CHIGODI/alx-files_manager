739fd6a395ff033609c0979e0f3c9141f001ceb4
LewisJassy <jassylewis70@gmail.com>
2024-09-02 23:42:21 +0300

    Refactor FileController to add getFile method for retrieving file content

--- controllers/FilesController.js
+++ controllers/FilesController.js
@@ -1,4 +1,5 @@
 const { v4: uuidv4 } = require('uuid');
+const mime = require('mime-types');
 const { ObjectId } = require('mongodb');
 const fs = require('fs-extra');
 const path = require('path');
@@ -118,6 +119,49 @@ class FileController {
       createdAt: newFile.createdAt,
       updatedAt: newFile.updatedAt,
     });
+  }
+
+  static async getFile(req, res) {
+    const { id } = req.params;
+    const userId = await redisClient.get(`auth_${req.headers['x-token']}`);
+
+    // Find the file document by ID
+    const fileDocument = await dbClient.db.collection('files').findOne({ _id: ObjectId(id) });
+
+    if (!fileDocument) {
+      return res.status(404).json({ error: 'Not found' });
+    }
+
+    // Check if the file is public or if the user is authenticated and is the owner
+    if (!fileDocument.isPublic && (!userId || fileDocument.userId.toString() !== userId)) {
+      return res.status(404).json({ error: 'Not found' });
+    }
+
+    // Check if the file type is folder
+    if (fileDocument.type === 'folder') {
+      return res.status(400).json({ error: "A folder doesn't have content" });
+    }
+
+    // Check if the file exists on disk
+    if (!fs.existsSync(fileDocument.localPath)) {
+      return res.status(404).json({ error: 'Not found' });
+    }
+
+    // Get the MIME type of the file based on its name
+    const mimeType = mime.lookup(fileDocument.name);
+
+    // Read and return the file content
+    fs.readFile(fileDocument.localPath, (err, data) => {
+      if (err) {
+        return res.status(500).json({ error: 'Unable to read file' });
+      }
+
+      res.setHeader('Content-Type', mimeType);
+      return res.send(data);
+    });
+
+    // Ensure a return statement at the end
+    return null;
   }
 }
 
