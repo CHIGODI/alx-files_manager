713cf0415e78506a69c5e449e8e663373ba43054
CHIGODI <chiegody254@gmail.com>
2024-09-03 10:59:46 +0300

    Addd Files controller, incomplete upload logic

--- routes/index.js
+++ routes/index.js
@@ -1,11 +1,12 @@
 #!/usr/bin/node
 
 const express = require('express');
-
 const router = express.Router();
+
 const  AppController = require('../controllers/AppController');
 const UsersController = require('../controllers/UsersController')
 const AuthController = require('../controllers/AuthController')
+const FilesController = require('../controllers/FilesController')
 
 router.get('/status', AppController.getStatus);
 router.get('/stats', AppController.getStats);
@@ -13,6 +14,7 @@ router.post('/users', UsersController.postNew);
 router.get('/connect', AuthController.getConnect);
 router.get('/disconnect', AuthController.getDisconnect);
 router.get('/users/me', UsersController.getMe);
+router.post('/files', FilesController.postUpload);
 
 
 module.exports = router;
