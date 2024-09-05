d329e1bef9cc7042b5f08ed1e952917d3cb4ccac
LewisJassy <jassylewis70@gmail.com>
2024-09-05 10:57:40 +0300

    Refactor AppController to use ES6 module syntax and fix import statements

--- controllers/AppController.js
+++ controllers/AppController.js
@@ -10,11 +10,17 @@ class AppController {
   static getStatus(request, response) {
     const status = {
       redis: redisClient.isAlive(),
-      db: dbClient.isAlive(),
+  /**
+   * should return the number of users and files in DB:
+   * { "users": 12, "files": 1231 }
+   *  with a status code 200
+   */
+  static async getStats(request, response) {
+    const stats = {
+      users: await dbClient.nbUsers(),
+      files: await dbClient.nbFiles(),
     };
-    response.status(200).send(status);
-  }
-
+    response.status(200).send(stats);
   /**
    * should return the number of users and files in DB:
    * { "users": 12, "files": 1231 }
