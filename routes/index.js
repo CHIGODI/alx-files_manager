44c6609d9e33663620e7cae0147eff743e73b602
LewisJassy <jassylewis70@gmail.com>
2024-09-05 11:09:41 +0300

    Refactor import statement in routes/index.js to fix UsersController import

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,6 @@
 import express from 'express';
 import AppController from '../controllers/AppController.js';
-import UsersController from '../controllers/UsersController.js.js';
+import UsersController from '../controllers/UsersController.js';
 import AuthController from '../controllers/AuthController.js';
 import FilesController from '../controllers/FilesController.js';
 
